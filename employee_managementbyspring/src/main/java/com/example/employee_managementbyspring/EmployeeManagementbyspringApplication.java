dbc484bca7d352b094f53912583cc02fab62d822
costof cultivation <eshwardasaraju9390@gmail.com>
2024-08-01 17:27:22 +0530

    uploading base project for employee management

---
+++ employee_managementbyspring/src/main/java/com/example/employee_managementbyspring/EmployeeManagementbyspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee_managementbyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementbyspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementbyspringApplication.class, args);
+	}
+
+}
---
+++ employee_managementbyspring/src/test/java/com/example/employee_managementbyspring/EmployeeManagementbyspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employee_managementbyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementbyspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
